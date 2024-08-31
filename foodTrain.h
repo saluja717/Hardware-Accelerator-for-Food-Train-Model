d536e6018e7bae44801d94d58598351b5d852bf6
saluja717 <salujamanpreet286@gmail.com>
2024-08-31 13:01:58 +0530

    Added

---
+++ foodTrain.h
@@ -0,0 +1,5 @@
+#pragma once 
+#include "./include/k2c_tensor_include.h" 
+void foodTrain(float input_1_input_array[],float activation_output_array[]);
+void foodTrain_initialize(); 
+void foodTrain_terminate(); 
